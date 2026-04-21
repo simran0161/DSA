d6fb513401dc0e9877b5ddb94ff135a452ec2188
Simran Singh <simran.singh@bounteous.com>
2026-04-21 10:12:03 +0530

    Time: 5 ms (35.16%), Space: 47 MB (10.92%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -6,22 +6,21 @@ class Solution {
         mp.put('X',10);
         mp.put('L',50);
         mp.put('C',100);
-        mp.put('D',500);
+        mp.put('D',500);  
         mp.put('M',1000);
 
         int res=0;
         for(int i=0;i<s.length();i++){
-            char c=s.charAt(i);
-            if(i+1<s.length() && mp.get(s.charAt(i+1))>mp.get(c)){
-                res+=mp.get(s.charAt(i+1))-mp.get(c);
+            char ch=s.charAt(i);
+            if(i+1<s.length() && mp.get(s.charAt(i+1))>mp.get(ch)){
+                res+=mp.get(s.charAt(i+1))-mp.get(ch);
                 i++;
-                System.out.println(res);
             }
             else{
-                res+=mp.get(c);
-                System.out.println(res);
+                res+=mp.get(ch);
             }
-        }
+        }  
         return res;
+
     }
 }
