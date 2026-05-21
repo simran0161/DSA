533068ac6ffb2ac9a1c71707b6a37de4246dd2c2
Simran Singh <simran.singh@bounteous.com>
2026-05-21 11:07:39 +0530

    Time: 8 ms (90.92%), Space: 49.2 MB (38.92%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+
+        ArrayList<int[]>res=new ArrayList<>();
+
+        res.add(intervals[0]);
+
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]<=res.get(res.size()-1)[1]){
+                res.get(res.size()-1)[1]=Math.max(res.get(res.size()-1)[1],intervals[i][1]);
+            }
+            else{
+                res.add(intervals[i]);
+            }
+        }
+        return res.toArray(new int[res.size()][]);
+    }
+}
