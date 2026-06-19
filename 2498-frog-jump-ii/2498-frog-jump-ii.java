0f227f5ec1e4909c99158c704b07a25eb88eed66
Simran Singh <simran.singh@bounteous.com>
2026-06-19 15:27:43 +0530

    Time: 3 ms (93.17%), Space: 82.9 MB (91.08%) - LeetHub

---
+++ 2498-frog-jump-ii/2498-frog-jump-ii.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxJump(int[] stones) {
+        int res=stones[1]-stones[0];
+        for(int i=2;i<stones.length;i+=2){
+            res=Math.max(res,stones[i]-stones[i-2]);
+        }
+        for(int i=3;i<stones.length;i+=2){
+            res=Math.max(res,stones[i]-stones[i-2]);
+        }
+        return res;
+    }
+}
