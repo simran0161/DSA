3d825b59dfcbf9fc2f2f8278ef1436944d896a5d
Simran Singh <simran.singh@bounteous.com>
2026-07-31 12:03:14 +0530

    Time: 39 ms (8.33%), Space: 47.3 MB (5.39%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -8,13 +8,12 @@ class Solution {
 
         while(j<n){
             mp.put(s.charAt(j),mp.getOrDefault(s.charAt(j),0)+1);
-
             while(mp.size()>2){
                 res+=n-j;
-                char rightchar=s.charAt(i);
-                mp.put(rightchar,mp.get(rightchar)-1);
-                if(mp.get(rightchar)==0){
-                    mp.remove(rightchar);
+                char c=s.charAt(i);
+                mp.put(c,mp.get(c)-1);
+                if(mp.get(c)==0){
+                    mp.remove(c);
                 }
                 i++;
             }
