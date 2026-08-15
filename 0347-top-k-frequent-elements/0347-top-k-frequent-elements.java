693ab9e841a23b00f33c7e764099bd1bbd5ea5ba
Simran Singh <simran.singh@bounteous.com>
2026-08-15 16:27:08 +0530

    Time: 14 ms (70.38%), Space: 47.3 MB (96.29%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,28 +1,32 @@
 class Pair{
-    int key, value;
-    public Pair(int key, int value){
+    int key;
+    int val;
+    Pair(int key,int val){
         this.key=key;
-        this.value=value;
+        this.val=val;
     }
 }
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        HashMap<Integer,Integer>mp=new HashMap<>();
-
+        int[] res=new int[k];
+        Map<Integer,Integer>mp=new HashMap<>();
         for(int i:nums){
             mp.put(i,mp.getOrDefault(i,0)+1);
         }
 
-        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->b.key-a.key);
+        PriorityQueue<Pair>pq=new PriorityQueue<>((a,b)->b.val-a.val);
+
         for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-            pq.offer(new Pair(entry.getValue(),entry.getKey()));
+            pq.add(new Pair(entry.getKey(),entry.getValue()));
         }
 
-        int[] res=new int[k];
-        for(int i=0;i<k;i++){
-            Pair pair=pq.poll();
-            res[i]=pair.value;
+        int c=0;
+        while(!pq.isEmpty() && c!=k){
+            Pair p=pq.poll();
+            res[c]=p.key;
+            c++;
         }
+
         return res;
     }
 }
