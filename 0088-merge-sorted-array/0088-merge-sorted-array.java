af86ef3d36c2477ebf9b7504b09e7103768a4245
Simran Singh <simran.singh@bounteous.com>
2026-09-02 14:58:58 +0530

    Time: 0 ms (100%), Space: 43.8 MB (47.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,21 +5,22 @@ class Solution {
         int k=m+n-1;
 
         while(i>=0 && j>=0){
-            if(nums1[i]<=nums2[j]){
-                nums1[k]=nums2[j];
-                k--;
-                j--;
-            }
-            else{
+            if(nums1[i]>nums2[j]){
                 nums1[k]=nums1[i];
                 i--;
                 k--;
             }
-        }
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            k--;
+            else{
+                nums1[k]=nums2[j];
+                j--;
+                k--;
+            }
+        } 
+
+        while(j>=0){
+            nums1[k]=nums2[j];
             j--;
+            k--;
         }
     }
 }
